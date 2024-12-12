f977a27872bb8003c88c304d75dffab4a7b6dc1d
Yeong Shian Poon <yeongshian.poon@timeteccloud.com>
2024-12-12 10:08:32 +0800

    -add retrieve distinct company_id

---
+++ ClockingScheduleCategory/DataAccess/ClockingScheduleDA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using App_General;
+using MySql.Data.MySqlClient;
+
+namespace ClockingScheduleCategory.DataAccess
+{
+    internal class ClockingScheduleDA
+    {
+        DBHelper _DBHelper;
+
+        internal ClockingScheduleDA(string connectionString)
+        {
+            _DBHelper = new DBHelper(connectionString);
+        }
+
+        internal DataTable GetDistinctCompanyIds()
+        {
+            DataTable dt = new DataTable();
+            string query = $@"SELECT DISTINCT s.Company_ID
+                              FROM schedule s
+                              WHERE NOT EXISTS (
+                                  SELECT 1
+                                  FROM schedule_category sc
+                                  WHERE s.Company_ID = sc.Company_ID
+                              );";
+
+            return _DBHelper.RetrieveRecord(query, new List<MySqlParameter>());
+        }
+    }
+}
--- ClockingScheduleCategory/Program.cs
+++ ClockingScheduleCategory/Program.cs
@@ -1,12 +1,73 @@
-﻿using System;
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using ClockingScheduleCategory.DataAccess;
+using ClockingScheduleCategory.Dto;
+using System.Data;
+using System.Linq;
 
 namespace ClockingScheduleCategory
 {
     internal class Program
     {
+        private static string _replicaConnectionString;
+        private static ClockingScheduleDA _clockingScheduleDA;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Clocking Category Start Running...");
+            string logDirectory = "Log";
+            Directory.CreateDirectory(logDirectory);
+
+            Log.Logger = new LoggerConfiguration()
+              .WriteTo.File($"Log/.txt", rollingInterval: RollingInterval.Day)
+              .CreateLogger();
+
+            Log.Information("Service started");
+
+            try
+            {
+                _replicaConnectionString = ConfigurationManager.ConnectionStrings["LocalConnectionString"]?.ConnectionString.ToString();
+
+                // Retrieve Company Id from schedule table
+                var companyIds = GetDistinctCompanyIds(_replicaConnectionString); //3394 company id
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An error occurred while running the service");
+            }
+            finally
+            {
+                Log.Information("Service stopped");
+
+                // Loop through company, add default sche categories
+
+
+                // Update schudule table with default schedule categories id
+
+
+            }
+        }
+
+        static List<int> GetDistinctCompanyIds(string replicaConnectionString)
+        {
+            _clockingScheduleDA = new ClockingScheduleDA(_replicaConnectionString);
+
+            var dtClockingSchedule = _clockingScheduleDA.GetDistinctCompanyIds();
+
+            return dtClockingSchedule.AsEnumerable()
+                                     .Select(dr => Convert.ToInt32(dr["Company_ID"]))
+                                     .ToList();
+            //    AutoNo = Convert.ToUInt64(dr["Auto_No"]),
+            //    CompanyId = dr["Company_ID"].ToString(),
+            //    UserID = Convert.ToUInt64(dr["UserID"]),
+            //    UID = Convert.ToUInt64(dr["UID"]),
+            //    CheckTime = Convert.ToDateTime(dr["CheckTime"]),
+            //    TerminalSn = dr["SerialNo"].ToString(),
+            //}).ToList();
+            //return new List<int>();
         }
     }
 }
